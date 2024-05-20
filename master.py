14de490bee81be370115e73a987dff147d6068dd
krylovolegv <krylovolegv@yandex.ru>
2024-05-20 21:40:05 +0200

    code_Search_DZ

--- master.py
+++ master.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 import random
+import time
 
 # Установка geckodriver
 service = FirefoxService(executable_path=GeckoDriverManager().install())
@@ -10,26 +11,59 @@ service = FirefoxService(executable_path=GeckoDriverManager().install())
 # Создаём объект браузера, через который мы будем действовать.
 browser = webdriver.Firefox(service=service)
 
-# Открываем сайт Википедии
-browser.get(
-    "https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
 
-# Ищем все элементы div с классом "hatnote navigation-not-searchable"
-hatnotes = []
-for element in browser.find_elements(By.TAG_NAME, "div"):
-    # Чтобы искать атрибут класса
-    cl = element.get_attribute("class")
-    if cl == "hatnote navigation-not-searchable":
-        hatnotes.append(element)
+def get_paragraphs():
+    paragraphs = browser.find_elements(By.TAG_NAME, "p")
+    return [p.text for p in paragraphs if p.text]
 
-if hatnotes:
-    # Выбираем случайный элемент из найденных
-    hatnote = random.choice(hatnotes)
 
-    # Для получения ссылки находим тег "a" внутри тега "div"
-    link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+def get_related_links():
+    links = []
+    # Ищем ссылки в элементах div с классом "hatnote navigation-not-searchable"
+    hatnotes = browser.find_elements(By.CSS_SELECTOR, "div.hatnote.navigation-not-searchable a")
+    links.extend([hatnote.get_attribute("href") for hatnote in hatnotes])
 
-    # Переходим по ссылке
-    browser.get(link)
+    # Также ищем все ссылки внутри основного контента статьи
+    content_links = browser.find_elements(By.CSS_SELECTOR, "#mw-content-text a[href^='/wiki/']")
+    for link in content_links:
+        href = link.get_attribute("href")
+        if href.startswith("https://ru.wikipedia.org/wiki/"):
+            links.append(href)
+
+    return links
+
+
+def main():
+    # Открываем сайт Википедии
+    browser.get(
+        "https://ru.wikipedia.org/wiki")
+
+    while True:
+        action = input("Выберите действие: 1 - Листать параграфы, 2 - Перейти на связанную страницу, 3 - Выйти: ")
+
+        if action == "1":
+            paragraphs = get_paragraphs()
+            for para in paragraphs:
+                print(para)
+                time.sleep(1)  # Даем немного времени для прочтения
+        elif action == "2":
+            links = get_related_links()
+            if links:
+                link = random.choice(links)
+                browser.get(link)
+                print(f"Переход по ссылке: {link}")
+            else:
+                print("Связанных страниц не найдено.")
+        elif action == "3":
+            break
+        else:
+            print("Некорректный ввод. Попробуйте снова.")
+
+    # Закрываем браузер
+    browser.quit()
+
+
+if __name__ == "__main__":
+    main()
 
 
